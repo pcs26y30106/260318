7afc914c6536d45833ddee5d52ef4c50559a9769
pcs26y30106 <26y30106@pcs.hs.kr>
2026-03-18 09:13:47 +0900

    second commit

--- 30106.py
+++ 30106.py
@@ -1,8 +1,8 @@
 class_number = 30106
 name = "박승민"
 
-print(class_number)
-print(name)
+print(class_number) # 30106
+print(name) # 박승민
 
-print(f"{class_number} {name}")
+print(f"{class_number} {name}") # 30106 박승민
 
